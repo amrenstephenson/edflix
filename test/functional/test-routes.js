4ab00d90fc0c14e58b938ea86c4dffec08e4dc4e
Muirey03 <tcmuir03@gmail.com>
2022-11-25 10:49:03 +0000

    Test failing test

--- test/functional/test-routes.js
+++ test/functional/test-routes.js
@@ -11,48 +11,10 @@ describe('GET /', () => {
     return request(server)
       .get('/')
       .expect('Content-Type', 'text/html; charset=UTF-8')
-      .expect(200)
+      .expect(404)
       .then(response => {
         expect(response.text).to.include(
           'Welcome to EDFLIX',
-        );
-      });
-  });
-});
-
-describe('GET /health', () => {
-  it('responds with json', () => {
-    return request(server)
-      .get('/health/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', 'application/json; charset=utf-8')
-      .expect(200, {
-        status: 'UP',
-      });
-  });
-});
-
-describe('GET /swagger/api-docs', () => {
-  it('responds with swagger', () => {
-    return request(server)
-      .get('/swagger/api-docs/')
-      .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(200)
-      .then(response => {
-        expect(response.text).to.include('Swagger');
-      });
-  });
-});
-
-describe('POST /fake/route', () => {
-  it('responds with not found page', () => {
-    return request(server)
-      .post('/fake/route')
-      .expect('Content-Type', 'text/html; charset=UTF-8')
-      .expect(200)
-      .then(response => {
-        expect(response.text).to.include(
-          'Whoops! Looks like you got lost or couldn\'t find your page.',
         );
       });
   });
