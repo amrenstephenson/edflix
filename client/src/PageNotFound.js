3be070c597451efc66e94051cb9b550eec8d62f4
Muirey03 <tcmuir03@gmail.com>
2022-12-08 18:37:01 +0000

    Routes for homepage and 404 page

---
+++ client/src/PageNotFound.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function PageNotFound() {
+	return (
+		<div className="App">
+			<h1>404!</h1>
+			<p>Whoops! Looks like you got lost or couldn't find your page.</p>
+			<p>Click to go to the <a href="/">overview page</a>.</p>
+		</div>
+	);
+}
