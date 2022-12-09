d83c8c60180894a8bcddd577b7ec9198d2ff95b3
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-12-09 10:57:09 +0000

    Add bootstrap navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,8 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={ <Home/> }/>
-				<Route path='*' element={ <PageNotFound /> }/>
+				<Route path="/" element={<Home />} />
+				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,34 @@
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Image } from 'react';
+
+function NavBar() {
+	return (
+		<Navbar bg="dark" variant="dark" expand="lg">
+			<Container>
+				<Navbar.Brand href="#home">
+					<img src="images/edflix-logo.png" style={{ height: "2rem" }} />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Link</Nav.Link>
+						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">
+								Another action
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">
+								Separated link
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default NavBar;
