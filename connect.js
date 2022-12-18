99660956cc2737f6d6bc1e1c0aa0be46e0045e37
J1ayi <vhmx35@durham.ac.uk>
2022-12-18 13:27:58 +0000

    connect.js

---
+++ connect.js
@@ -0,0 +1,29 @@
+const sqlite3 = require('sqlite3').verbose();
+
+
+let db = new sqlite3.Database('./Edflix.db',sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the Edflix SQlite database.');
+  });
+
+  db.serialize(() => {
+    db.each(`SELECT Artifact_id as id,
+                    Artifact_Name as name
+             FROM Artifact`, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log(row.id + "\t" + row.name);
+    });
+  });
+
+
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+
