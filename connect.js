bc46f6d7ea4617b68e0e702c7ab99514130b5b86
J1ayi <vhmx35@durham.ac.uk>
2022-12-18 13:53:22 +0000

    connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 
-let db = new sqlite3.Database('./Edflix.db',sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./Edflix.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       return console.error(err.message);
     }
