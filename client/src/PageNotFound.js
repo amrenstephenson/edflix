d0c02305611265e2aec9b7775661cea9491cde88
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-12-19 16:42:11 +0000

    Remove unused class name

--- client/src/PageNotFound.js
+++ client/src/PageNotFound.js
@@ -1,6 +1,6 @@
 export default function PageNotFound() {
 	return (
-		<div className="App">
+		<div>
 			<h1>404!</h1>
 			<p>Whoops! Looks like you got lost or couldn't find your page.</p>
 			<p>Click to go to the <a href="/">overview page</a>.</p>
