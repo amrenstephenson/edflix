1073e48d4cb54d5115706f469c71eeda3dd98f14
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-12-19 16:12:05 +0000

    Add rows of artifacts to homepage

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,15 +1,22 @@
-import './App.css';
+import ArtifactBanner from './components/ArtifactBanner';
+import ArtifactRow from './components/ArtifactRow';
 import NavBar from './components/NavBar';
 
 export default function Home() {
 	return (
-		<div className="App">
+		<div>
 			<header>
-				<NavBar></NavBar>
+				<NavBar />
 			</header>
 			<main>
-				<br></br>
-				<p>Welcome to EDFLIX</p>
+				<ArtifactBanner />
+				<div style={{ padding: '2rem', paddingTop: '3rem' }}>
+					<ArtifactRow title='Personalised Recommendations' />
+					<ArtifactRow title='Trending' />
+					<ArtifactRow title='IBM Z' />
+					<ArtifactRow title='IBM Cloud' />
+					<ArtifactRow title='Engineering' />
+				</div>
 			</main>
 		</div>
 	);
