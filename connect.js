eacd4b079b284722458120338dcdd845c19d5afa
J1ayi <vhmx35@durham.ac.uk>
2022-12-26 17:11:58 +0000

    connect.js

--- connect.js
+++ connect.js
@@ -1,24 +1,77 @@
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('./Edflix.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the Edflix SQlite database.');
-});
-db.serialize(() => {
-  db.each(`SELECT Artifact_id as id,
-                    Artifact_Name as name
-             FROM Artifact`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(row.id + '\t' + row.name);
-  });
-});
-db.close((err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
+import sqlite3 from 'sqlite3'
+const sqlite = sqlite3.verbose()
 
+class Sqlite {
+  constructor() {
+    this.instance
+    this.db = null
+  }
+  
+  connect(path='./Edflix.db') {
+    return new Promise((resolve,reject) => {
+      this.db = new sqlite.Database(path, (err) => {
+        if(err === null) {
+          resolve(err)
+        } else {
+          reject(err)
+        }
+      })
+      
+    })
+  }
+
+  run(sql, params) {
+    return new Promise((resolve, reject) => {
+      this.db.run(sql, params, (err) => {
+        if(err === null) {
+          resolve(err)
+        } else {
+          reject(err)
+        }
+      })
+    })
+  }
+
+  exec(sql) {
+    return new Promise((resolve, reject) => {
+      this.db.exec(sql, (err) => {
+        if(err === null) {
+          resolve(err)
+        } else {
+          reject(err)
+        }
+      })
+    })
+  }
+
+  get(sql, params) {
+    return new Promise((resolve, reject) => {
+      this.db.get(sql, params, (err, data) => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  }
+
+  all(sql, params) {
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, params, (err, data) => {
+        if(err) {
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      })
+    })
+  }
+
+  close() {
+    this.db.close()
+  }
+
+}
+
+export default Sqlite
