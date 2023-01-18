5493c63153ebb07722ed264aa44edd520e9208f2
Muirey03 <tcmuir03@gmail.com>
2023-01-18 19:04:43 +0000

    Basic recommendation algorithm using journal similarity func

--- server/algorithm/algorithm.js
+++ server/algorithm/algorithm.js
@@ -2,13 +2,54 @@ import Sqlite from '../db/connect.js';
 import fs from 'fs';
 import { runInContainer, createExampleUsers } from './container.js';
 
+async function getAllJournalsAndUsers() {
+  return await db.all(
+    // eslint-disable-next-line max-len
+    'SELECT * FROM LearningJournal j INNER JOIN User u ON u.Journal_id = j.Journal_id',
+  );
+}
+
+function calculateJournalSimilarity(journal1, journal2) {
+  if (journal1 === journal2) {
+    return 0.0;
+  }
+
+  // TODO: improve
+  let score = 0.0;
+  if (journal1.LevelOfStudy === journal2.LevelOfStudy)
+    score += 0.2;
+  if (journal1.UniversityCourse === journal2.UniversityCourse)
+    score += 0.5;
+  if (journal1.University === journal2.University)
+    score += 0.1;
+  // TODO: use journal's module list
+  return score;
+}
+
 async function getSimilarJournals(journalID) {
-  return [];
+  const journals = await getAllJournalsAndUsers();
+  const userJournal = journals.find(j => j.Journal_id === journalID);
+  const similarities = journals.map(j => (
+    {
+      journalID: j.Journal_id,
+      userID: j.User_id,
+      score: calculateJournalSimilarity(userJournal, j),
+    }
+  ));
+  return similarities;
+}
+
+var db = null;
+function initialiseDBConnection() {
+  db = new Sqlite();
+  db.connect();
 }
 
 async function getRecommendedArtifacts(userID) {
-  const db = new Sqlite();
-  db.connect();
+  if (!db) {
+    initialiseDBConnection();
+  }
+
   const journalID = (await db.get(
     'SELECT Journal_id FROM User WHERE User_id=?',
     [userID],
@@ -16,25 +57,25 @@ async function getRecommendedArtifacts(userID) {
 
   const scaleRating = (val) => (val - 2.5) / 2.5;
 
-  const similarUsers = await getSimilarJournals(journalID);
+  const similarJournals = await getSimilarJournals(journalID);
 
   let artifactScores = {};
-  for (const otherUser of similarUsers) {
-    const userRatings = await db.all(
-      'SELECT * FROM Rating WHERE User_id=?', [otherUser.id],
+  for (const otherJournal of similarJournals) {
+    const otherUserRatings = await db.all(
+      'SELECT * FROM Rating WHERE User_id=?', [otherJournal.userID],
     );
-    for (const rating of userRatings) {
-      const oldScore = (artifactScores[rating.Artifact_id] === undefined) ?
-        0.0 :
-        artifactScores[rating.Artifact_id];
+    for (const rating of otherUserRatings) {
+      const oldScore = artifactScores[rating.Artifact_id] || 0.0;
 
       artifactScores[rating.Artifact_id] =
-        oldScore + otherUser.score * scaleRating(rating.Value);
+        oldScore + otherJournal.score * scaleRating(rating.Value);
     }
   }
   db.close();
 
-  return artifactScores;
+  return Object.entries(artifactScores).sort((a, b) => (
+    a[1] < b[1] ? 1 : -1
+  ));
 }
 
 function main() {
