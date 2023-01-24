16938b81a336e343a031e52cc3e25dcd362bba49
yiqingliu76 <mnrh53@durham.ac.uk>
2023-01-24 17:55:55 +0000

    Changes

--- server/controllers/api-controller.js
+++ server/controllers/api-controller.js
@@ -18,6 +18,79 @@ class APIController {
     }
 
   };
+  getArtifacts_id = async(req, res) => {
+    let id = req.params['id'];
+    try {
+      // eslint-disable-next-line max-len
+      let artifacts = await this.db.all(`SELECT * FROM Artifact where Artifact_id=${id}`);
+      res.json(artifacts);
+    } catch (e) {
+      console.log(e);
+      res
+        .status(500)
+        .send('Internal Server Error - Could not get artifacts from database.');
+    }
+
+  };
+  getArtifacts_Name = async(req, res) => {
+    let Name = req.params['Name'];
+    try {
+      // eslint-disable-next-line max-len
+      let artifacts = await this.db.all(`SELECT * FROM Artifact where Artifact_Name=${Name}`);
+      res.json(artifacts);
+    } catch (e) {
+      console.log(e);
+      res
+        .status(500)
+        .send('Internal Server Error - Could not get artifacts from database.');
+    }
+
+  };
+  getArtifacts_avag = async(req, res) => {
+    let id = req.params['id'];
+    try {
+      // eslint-disable-next-line max-len
+      let artifacts = await this.db.all(`SELECT * FROM Artifact where Artifact_id=${id}`);
+      res.json(artifacts);
+    } catch (e) {
+      console.log(e);
+      res
+        .status(500)
+        .send('Internal Server Error - Could not get artifacts from database.');
+    }
+
+  };
+  login = async(req, res) => {
+
+    let {userName, passWorld} = req.body;
+    try {
+      // eslint-disable-next-line max-len
+      let artifacts = await this.db.all(`SELECT * FROM User where User_name='${userName}' and  Password= '${passWorld}'`);
+      res.json(artifacts);
+    } catch (e) {
+      console.log(e);
+      res
+        .status(500)
+        .send('Internal Server Error - Could not get artifacts from database.');
+    }
+  };
+  register = async(req, res) => {
+    let {Email, passWorld, userName} = req.body;
+    try {
+      // eslint-disable-next-line max-len
+      let artifacts = await this.db.all(`SELECT * FROM User where User_name='${userName}' and  Password= '${passWorld}' and Email='${Email}'`);
+      res.json(artifacts);
+    } catch (e) {
+      console.log(e);
+      res
+        .status(500)
+        .send('Internal Server Error - Could not get artifacts from database.');
+    }
+
+  };
+
+
 }
+
 
 export default APIController;
--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -6,4 +6,20 @@ const apiController = new APIController();
 
 router.get('/artifacts', apiController.getArtifacts);
 
+router.get('/artifacts', apiController.getArtifacts_id);
+
+router.get('/artifacts', apiController.getArtifacts_Name);
+
+router.get('/api/ratings/get/artifact-id',apiController.getArtifacts_avag)
+
+router.get('/recommendations/get/<user_id>', apiController.recommendations_id);
+
+router.post('/login', apiController.login);
+
+router.post('/register ', apiController.register);
+
+.
+
+
+
 export default router;
