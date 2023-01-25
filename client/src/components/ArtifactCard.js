d2d9c1f35b64fa47498d6277bdf72d2b1eefa29e
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-01-25 16:51:29 +0000

    Center artifact background image

--- client/src/components/ArtifactCard.js
+++ client/src/components/ArtifactCard.js
@@ -1,6 +1,6 @@
 function ArtifactCard(props) {
     return (
-        <div style={{ borderRadius: '2rem', background: `url("${props.artifact.ThumbnailURL}")`, backgroundSize: 'cover', border: '0.5px solid white', ...props.style }}>
+        <div style={{ borderRadius: '2rem', background: `url("${props.artifact.ThumbnailURL}")`, backgroundSize: 'cover', backgroundPosition: 'center', border: '0.5px solid white', ...props.style }}>
             <div style={{borderRadius: '2rem', background: '#00000044', width: '100%', height: '100%', padding: '1rem'}}>
                 <b>{props.artifact.Artifact_Name}</b>
             </div>
