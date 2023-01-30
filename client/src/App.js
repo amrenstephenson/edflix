5ecd38007d02542af153ba7b9004ebd94c81b365
hengruixu <13910357579@163.com>
2023-01-30 09:25:10 +0000

    message

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" exact element={<Home />} />
 				<Route path="/login" exact element={<Login />} />
-				<Route path="/signup" exact element={<Signup />} />
+				<Route path="/sign" exact element={<Signup />} />
 				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
