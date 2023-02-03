3fd6dad00b90fb7f6414c2803938f9f805d0fe85
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-02-03 09:40:07 +0000

    Add start script

---
+++ start-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILENAME="start-dev.sh"
+START="\033[31;1;4m"
+END="\033[0m"
+printf "${START}[${FILENAME}]${END} Starting server" &&
+npm start &
+sleep 2 &&
+printf "${START}[${FILENAME}]${END} Starting client" &&
+(cd client && npm start)
