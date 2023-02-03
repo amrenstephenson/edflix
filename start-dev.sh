0bcea5c732a5e76fec8cf6d51c870acecd16a26a
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-02-03 10:28:52 +0000

    Add run dev

--- start-dev.sh
+++ start-dev.sh
@@ -3,7 +3,7 @@ FILENAME="start-dev.sh"
 START="\033[31;1;4m"
 END="\033[0m"
 printf "${START}[${FILENAME}]${END} Starting server" &&
-npm start &
+npm run dev &
 sleep 2 &&
 printf "${START}[${FILENAME}]${END} Starting client" &&
 (cd client && npm start)
