b1564f25c6d5ef300105ba0f2032c23d5e9b0ff8
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-02-17 10:23:38 +0000

    NavBar changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,45 +1,63 @@
-import { Navbar, Nav, NavDropdown, Container, Form, InputGroup } from "react-bootstrap";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import { Navbar, Nav, NavDropdown, Container, Form, InputGroup } from 'react-bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useEffect, useState } from 'react';
+import { serverURL } from '..';
 
 function NavBar() {
-	return (
-		<Navbar bg="dark" variant="dark" expand="lg">
-			<Container>
-				<Navbar.Brand href="/">
-					<img src="images/edflix-logo.png" alt="Edflix logo" style={{ height: "2rem" }} />
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link href="/" active>Home</Nav.Link>
-						<Nav.Link href="#link">Link</Nav.Link>
-						<Nav.Link href="/signup">Sign Up</Nav.Link>
-						<Nav.Link href="/LearningJournal">LearningJournal</Nav.Link>
-						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">
-								Another action
-							</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">
-								Separated link
-							</NavDropdown.Item>
-						</NavDropdown>
-					</Nav>
+	const [user, setUser] = useState({});
+    useEffect(() => {
+        const fetchUser = async () => {
+            const res = fetch(`${serverURL}/api/user`);
+            setUser((await res).json())
+        }
+        
+        fetchUser()
+            .catch(console.error);
+    }, [])
 
-					<Form className="d-flex">
-						<InputGroup>
-							<InputGroup.Text>
-								<i className="bi-search"></i>
-							</InputGroup.Text>
-							<Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" name="filter" />
-						</InputGroup>
-					</Form>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img src="images/edflix-logo.png" alt="Edflix logo" style={{ height: '2rem' }} />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav d-flex">
+                    <Nav className="flex-grow-1">
+                        <Nav.Link href="/" active={window.location.pathname === '/'}>
+                            Home
+                        </Nav.Link>
+                        <Nav.Link href="/learning-journal" active={window.location.pathname === '/learning-journal'}>
+                            Learning Journal
+                        </Nav.Link>
+                    </Nav>
+
+                    <Nav className="flex-grow-1 justify-content-end">
+                        <Nav.Link href="/login" active={window.location.pathname === '/login'}>
+                            Login
+                        </Nav.Link>
+                        <Nav.Link href="/signup" active={window.location.pathname === '/signup'}>
+                            Sign Up
+                        </Nav.Link>
+                        {window.location.pathname !== '/login' && window.location.pathname !== '/signup' && (
+                            <>
+                                <div style={{ width: 20, height: 20 }} />
+                                <Form>
+                                    <InputGroup>
+                                        <InputGroup.Text>
+                                            <i className="bi-search"></i>
+                                        </InputGroup.Text>
+                                        <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" name="filter" />
+                                    </InputGroup>
+                                </Form>
+                                <div style={{ height: 20 }} />
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default NavBar;
