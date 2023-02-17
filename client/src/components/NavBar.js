25ff1aab8e40ea4a3f49a44d21a9df161a80df16
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-02-17 11:08:00 +0000

    Make search bar cool

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,11 @@
-import { Navbar, Nav, NavDropdown, Container, Form, InputGroup } from 'react-bootstrap';
+import { Navbar, Nav, Container, Form, InputGroup } from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { useEffect, useState } from 'react';
 import { serverURL } from '..';
+import './NavBar.css';
 
 function NavBar() {
-	const [user, setUser] = useState({});
+	const [, setUser] = useState({});
     useEffect(() => {
         const fetchUser = async () => {
             const res = fetch(`${serverURL}/api/user`);
@@ -23,7 +24,7 @@ function NavBar() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav d-flex">
-                    <Nav className="flex-grow-1">
+                    <Nav>
                         <Nav.Link href="/" active={window.location.pathname === '/'}>
                             Home
                         </Nav.Link>
@@ -33,26 +34,26 @@ function NavBar() {
                     </Nav>
 
                     <Nav className="flex-grow-1 justify-content-end">
+                        {window.location.pathname !== '/login' && window.location.pathname !== '/signup' && (
+                            <>
+                                <div style={{ width: 10, height: 20 }} />
+                                <Form className="flex-grow-1 search-bar">
+                                    <InputGroup>
+                                        <InputGroup.Text>
+                                            <i className="bi-search"></i>
+                                        </InputGroup.Text>
+                                        <Form.Control type="search" placeholder="Search" className="me-2 search-input" aria-label="Search" name="filter" />
+                                    </InputGroup>
+                                </Form>
+                                <div style={{ height: 20 }} />
+                            </>
+                        )}
                         <Nav.Link href="/login" active={window.location.pathname === '/login'}>
                             Login
                         </Nav.Link>
                         <Nav.Link href="/signup" active={window.location.pathname === '/signup'}>
                             Sign Up
                         </Nav.Link>
-                        {window.location.pathname !== '/login' && window.location.pathname !== '/signup' && (
-                            <>
-                                <div style={{ width: 20, height: 20 }} />
-                                <Form>
-                                    <InputGroup>
-                                        <InputGroup.Text>
-                                            <i className="bi-search"></i>
-                                        </InputGroup.Text>
-                                        <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" name="filter" />
-                                    </InputGroup>
-                                </Form>
-                                <div style={{ height: 20 }} />
-                            </>
-                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
