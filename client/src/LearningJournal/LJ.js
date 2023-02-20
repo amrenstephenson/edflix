f594b4a5770835a3407f9ad7d43fab8867c557e3
Muirey03 <tcmuir03@gmail.com>
2023-02-20 21:29:20 +0000

    Begin connecting learning journal to REST API

--- client/src/LearningJournal/LJ.js
+++ client/src/LearningJournal/LJ.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../components/NavBar';
 import './LJ.css'
 import { Button, Tag,Modal,Input,List,message, Avatar,Drawer,Space,Upload,Select, Popconfirm, Rate } from "antd";
 import {LinkOutlined,EditFilled, UploadOutlined} from "@ant-design/icons";
+import { serverURL } from '../index';
 
 
 const modules = Array.from({ length: 6 }).map((_, i) => `module ${i}`);
@@ -58,7 +59,7 @@ const Degree = [
   { label: "other" }
 ];
 
-const stage = [
+const stages = [
   { label: "year 1" },
   { label: "year 2" },
   { label: "year 3" },
@@ -87,20 +88,61 @@ const data = Array.from({ length: 21 }).map((_, i) => ({
 }));
 
 
+function UserInfo(props) {
+  const { userInfo } = props;
 
-export default function LearningJournal() {
-  const defaultUserInfo = {};
+  return (
+    <thead className="basicinfo">
+      <tr>
+        <th rowspan="2" className="username_title">
+          <div className="username">{userInfo.User_name}</div>
+          <div className="ProfilePicture">
+            <Avatar size={56} src={userinfomation[0].picture} />
+          </div>
+        </th>
+      </tr>
+      <tr>
+        <th className="email_title">Email:</th>
+        <th className="email">{userInfo.Email}</th>
+      </tr>
+    </thead>
+  );
+}
 
-  const [open, setOpen] = useState(false);
-  const [userInfo, setUserInfo] = useState(defaultUserInfo);
+function JournalInfo(props) {
+  const { journalInfo } = props;
 
-  const showDrawer = () => {
-    setOpen(true);
-  };
+  return (
+    <tbody>
+      <tr>
+        <td className="course_title">Course</td>
+        <td className="degree_title">Level of study</td>
+        <td className="modules_title">Modules</td>
+      </tr>
+      <tr>
+        <td className="course">{journalInfo.UniversityCourse}</td>
+        <td className="degree">
+          Level {journalInfo.LevelOfStudy}
+          <br />
+        </td>
+        <td className="modules">
+          {(journalInfo.modules.map((module) => (
+            <Tag color="blue">
+              {module}
+            </Tag>
+          )))}
+          
+          <Tag color="blue">
+            <a style={{ color: "blue" }}>...</a>
+          </Tag>
+        </td>
+      </tr>
+    </tbody>
+  );
+}
 
-  const onClose = () => {
-    setOpen(false);
-  };
+function EditDrawer(props) {
+  const { userInfo, journalInfo, open, setOpen } = props;
 
   const [loading, setLoading] = useState(false);
   const [Modalopen, ModalsetOpen] = useState(false);
@@ -114,238 +156,159 @@ export default function LearningJournal() {
     setTimeout(() => {
       setLoading(false);
       ModalsetOpen(false);
-    }, 3000);
+    }, 3000); //TODO: remove arbitrary 3 second wait
   };
 
   const handleCancel = () => {
     ModalsetOpen(false);
   };
 
+  const onClose = () => {
+    setOpen(false);
+  };
 
-  return(
-    <div>
-      <div>
-        <NavBar />
-      </div>
-      <div 
-        style={{
-          backgroundImage: 'linear-gradient(0, black, transparent), url("images/testing/learningJ.jpg")',
-          backgroundSize: '100%',
-          height: '20rem',
-          width: '100%',
-        }}
-      />
-      <div>
-        <table className="userinfo">
-          <thead className="basicinfo">
+  return (
+    <Drawer
+      title="Your Information"
+      placement="right"
+      onClose={onClose}
+      open={open}
+      size='large'
+      extra={
+        <Space>
+          <Button onClick={onClose} type="primary">
+                  Submit
+          </Button>
+        </Space>
+      }
+    >
+      <div style={{ width: "100%" }}>
+        <table style={{ width: "100%" }}>
+          <thead>
             <tr>
-              <th rowspan="2" className="username_title">
-                <div className="username">{userinfomation[0].username}</div>
-                <div className="ProfilePicture">
-                  <Avatar size={56} src={userinfomation[0].picture} />
-                </div>
+              <th rowspan="2" className="edit_picture">
+                <Avatar size={56} src={userinfomation[0].picture} />
               </th>
-              <th className="id_title">Id:</th>
-              <th className="id">{userinfomation[0].id}</th>
-            </tr>
-            <tr>
-              <th className="email_title">Email:</th>
-              <th className="email">{userinfomation[0].email}</th>
+              <th className="edit_basic_info">Username:</th>
+              <th className="inputbox">
+                <Input
+                  placeholder="Username"
+                  defaultValue={userInfo.User_name}
+                  maxLength="30"
+                  showCount="true"
+                  size="small"
+                />
+              </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="course_title">course</td>
-              <td className="degree_title">degree</td>
-              <td className="modules_title">modules</td>
-            </tr>
-            <tr>
-              <td className="course">{userinfomation[0].course}</td>
-              <td className="degree">
-                {userinfomation[0].degree}
-                <br />
-                {userinfomation[0].stage}
+              <td className="submit_picture">
+                <Upload {...props}>
+                  <Button size="small" icon={<UploadOutlined />}>
+                          Upload
+                  </Button>
+                </Upload>
               </td>
-              <td className="modules">
-                {userinfomation[0].module.map((module) => (
-                  <Tag color="blue" key={module}>
-                    {module}
-                  </Tag>
-                ))}
-                <Tag color="blue">
-                  <a style={{ color: "blue" }}>...</a>
-                </Tag>
+              <td className="edit_basic_info">Email:</td>
+              <td className="inputbox">
+                <Input
+                  placeholder="Email"
+                  defaultValue={userInfo.Email}
+                  maxLength="30"
+                  showCount="true"
+                  size="small"
+                />
               </td>
             </tr>
           </tbody>
         </table>
         <br />
-        <div style={{ width: "90%", textAlign: "right" }}>
-          <Button
-            type="primary"
-            shape="round"
-            icon={<EditFilled />}
-            onClick={showDrawer}
+      </div>
+      <div>
+        <h1 className="edit_course_degree_module_title">Course:</h1>
+        <Select
+          showSearch
+          style={{ width: "60%" }}
+          placeholder="Select a course"
+          defaultValue={journalInfo.UniversityCourse}
+          optionFilterProp="children"
+          filterOption={(input, option) =>
+            (option?.label ?? "").includes(input)
+          }
+          filterSort={(optionA, optionB) =>
+            (optionA?.label ?? "")
+              .toLowerCase()
+              .localeCompare((optionB?.label ?? "").toLowerCase())
+          }
+          options={courses}
+        />
+      </div>
+      <br />
+      <div>
+        <h2 className="edit_course_degree_module_title">Degree:</h2>
+        <Select
+          defaultValue={userinfomation[0].degree}
+          style={{ width: "60%" }}
+          options={Degree}
+        />
+        <Select
+          defaultValue={stages[parseInt(journalInfo.LevelOfStudy, 10) - 1]}
+          style={{ width: "30%" }}
+          options={stages}
+        />
+      </div>
+      <br />
+      <div>
+        <h3 className="edit_course_degree_module_title">Modules:</h3>
+        <div>
+          {journalInfo.modules.map((module) => (
+            <Tag color="blue">
+              {module}
+            </Tag>
+          ))}
+          <Tag color="blue">
+            <a style={{ color: "blue" }} onClick={showModal}>+</a>
+          </Tag>
+          <Modal
+            open={Modalopen}
+            title="Module selection"
+            onOk={handleOk}
+            onCancel={handleCancel}
+            footer={[
+              <Button key="back" onClick={handleCancel}>
+                      Return
+              </Button>,
+              <Button
+                key="submit"
+                type="primary"
+                loading={loading}
+                onClick={handleOk}
+              >
+                      Submit
+              </Button>
+            ]}
           >
-                  Edit Your information
-          </Button>
-          <Drawer
-            title="Your Information"
-            placement="right"
-            onClose={onClose}
-            open={open}
-            size='large'
-            extra={
-              <Space>
-                <Button onClick={onClose} type="primary">
-                        Submit
-                </Button>
-              </Space>
-            }
-          >
-            <div style={{ width: "100%" }}>
-              <table style={{ width: "100%" }}>
-                <thead>
-                  <tr>
-                    <th rowspan="2" className="edit_picture">
-                      <Avatar size={56} src={userinfomation[0].picture} />
-                    </th>
-                    <th className="edit_basic_info">Username:</th>
-                    <th className="inputbox">
-                      <Input
-                        placeholder="Username"
-                        defaultValue={userinfomation[0].username}
-                        maxLength="30"
-                        showCount="true"
-                        size="small"
-                      />
-                    </th>
-                  </tr>
-                  <tr>
-                    <th className="edit_basic_info">ID:</th>
-                    <th className="inputbox">
-                      <Input
-                        placeholder="ID"
-                        defaultValue={userinfomation[0].id}
-                        maxLength="30"
-                        showCount="true"
-                        size="small"
-                      />
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="submit_picture">
-                      <Upload {...props}>
-                        <Button size="small" icon={<UploadOutlined />}>
-                                Upload
-                        </Button>
-                      </Upload>
-                    </td>
-                    <td className="edit_basic_info">Email:</td>
-                    <td className="inputbox">
-                      <Input
-                        placeholder="Email"
-                        defaultValue={userinfomation[0].email}
-                        maxLength="30"
-                        showCount="true"
-                        size="small"
-                      />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <br />
-            </div>
-            <div>
-              <h1 className="edit_course_degree_module_title">Course:</h1>
-              <Select
-                showSearch
-                style={{ width: "60%" }}
-                placeholder="Select a course"
-                defaultValue={userinfomation[0].course}
-                optionFilterProp="children"
-                filterOption={(input, option) =>
-                  (option?.label ?? "").includes(input)
-                }
-                filterSort={(optionA, optionB) =>
-                  (optionA?.label ?? "")
-                    .toLowerCase()
-                    .localeCompare((optionB?.label ?? "").toLowerCase())
-                }
-                options={courses}
-              />
-            </div>
-            <br />
-            <div>
-              <h2 className="edit_course_degree_module_title">Degree:</h2>
-              <Select
-                defaultValue={userinfomation[0].degree}
-                style={{ width: "60%" }}
-                options={Degree}
-              />
-              <Select
-                defaultValue={userinfomation[0].stage}
-                style={{ width: "30%" }}
-                options={stage}
-              />
-            </div>
-            <br />
-            <div>
-              <h3 className="edit_course_degree_module_title">Modules:</h3>
-              <div>
-                {userinfomation[0].module.map((module) => (
-                  <Tag color="blue" key={module}>
-                    {module}
-                  </Tag>
-                ))}
-                <Tag color="blue">
-                  <a style={{ color: "blue" }} onClick={showModal}>
-                          +
-                  </a>
-                </Tag>
-                <Modal
-                  open={Modalopen}
-                  title="Module selection"
-                  onOk={handleOk}
-                  onCancel={handleCancel}
-                  footer={[
-                    <Button key="back" onClick={handleCancel}>
-                            Return
-                    </Button>,
-                    <Button
-                      key="submit"
-                      type="primary"
-                      loading={loading}
-                      onClick={handleOk}
-                    >
-                            Submit
-                    </Button>
-                  ]}
-                >
-                  <Input
-                    placeholder="New module"
-                    showCount="true"
-                    maxLength="30"
-                  />
-                </Modal>
-              </div>
-            </div>
-          </Drawer>
+            <Input
+              placeholder="New module"
+              showCount="true"
+              maxLength="30"
+            />
+          </Modal>
         </div>
       </div>
+    </Drawer>
+  );
+}
 
-
-
-
-
-
+function RatingsList(props) {
+  return (
+    <div>
       <div className='title' style={{textAlign:'center',marginTop:'2rem',marginBottom:'2rem'}}>
-              Your Learning Journal
+        Your Learning Journal
       </div>
 
-      <div style={{ background:' rgb(30, 30, 30)',width:'80%',marginLeft:'10%' }}>
+      <div style={{ background:'rgb(30, 30, 30)',width:'80%',marginLeft:'10%' }}>
         <List
           tyle={{ color:'white'}}
           itemLayout="vertical"
@@ -374,7 +337,6 @@ export default function LearningJournal() {
                   width={250}
                   alt="logo"
                   src="images/testing/artifiact-image.png"
-
                 />
               }
             >
@@ -387,8 +349,76 @@ export default function LearningJournal() {
           )}
         />
       </div>
-
     </div>
-  )
+  );
+}
 
+
+export default function LearningJournal() {
+  const defaultUserInfo = {};
+  const defaultJournalInfo = {modules: []};
+
+  const [userInfo, setUserInfo] = useState(defaultUserInfo);
+  const [journalInfo, setJournalInfo] = useState(defaultJournalInfo);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${serverURL}/api/user`);
+      if (res.status === 200) {
+        setUserInfo(await res.json());
+      }
+    }
+    fetchData().catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${serverURL}/api/journal`);
+      if (res.status === 200) {
+        setJournalInfo(await res.json());
+      }
+    }
+    fetchData().catch(console.error);
+  }, []);
+
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  return(
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div 
+        style={{
+          backgroundImage: 'linear-gradient(0, black, transparent), url("images/testing/learningJ.jpg")',
+          backgroundSize: '100%',
+          height: '20rem',
+          width: '100%',
+        }}
+      />
+      <div>
+        <table className="userinfo">
+          <UserInfo userInfo={ userInfo } />
+          <JournalInfo journalInfo={ journalInfo } />
+        </table>
+        <br />
+        <div style={{ width: "90%", textAlign: "right" }}>
+          <Button
+            type="primary"
+            shape="round"
+            icon={<EditFilled />}
+            onClick={showDrawer}
+          >
+                  Edit Your information
+          </Button>
+          <EditDrawer userInfo={ userInfo } journalInfo={ journalInfo } open={ open } setOpen={ setOpen } />
+        </div>
+      </div>
+
+      <RatingsList />
+    </div>
+  );
 }
