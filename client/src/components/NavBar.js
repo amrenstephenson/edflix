95d9b43da1543eb62faf37f15195170c10e4039c
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-02-21 15:50:11 +0000

    Fix logout dropdown displaying off-screen

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -70,6 +70,7 @@ function NavBar() {
                                     </span>
                                 }
                                 id="navbarScrollingDropdown"
+                                align="end"
                             >
                                 <div style={{ paddingTop: '0.5rem', paddingBottom: '0.75rem', paddingLeft: '1rem', paddingRight: '1rem', borderBottom: '1px solid hsl(210, 11%, 25%)', marginBottom: '0.5rem' }}>
                                     Logged in as
