29b2d642118dbee5030bd1c99c07f04f8782791e
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2023-02-22 18:14:06 +0000

    Make popup display based on search params

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,19 +1,23 @@
 import ArtifactBanner from './components/ArtifactBanner';
 import ArtifactRows from './components/ArtifactRows';
 import NavBar from './components/NavBar';
+import ArtifactPopup from './components/popup/ArtifactPopup';
+import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
-	return (
-		<div>
-			<header>
-				<NavBar />
-			</header>
-			<main className='container-fluid'>
-				<ArtifactBanner />
-				<div style={{ padding: '2rem', paddingTop: '3rem' }}>
-					<ArtifactRows />
-				</div>
-			</main>
-		</div>
-	);
+	const [params,] = useSearchParams();
+    return (
+        <div>
+            <header>
+                <NavBar />
+            </header>
+            <main className="container-fluid">
+                <ArtifactBanner />
+                <div style={{ padding: '2rem', paddingTop: '3rem' }}>
+                    <ArtifactRows />
+                </div>
+            </main>
+            {params.has('artifact') && params.has('topic') && <ArtifactPopup artifactID={params.get('artifact')} topic={params.get('topic')} />}
+        </div>
+    );
 }
